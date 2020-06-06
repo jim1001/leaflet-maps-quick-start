57b2726c459b6dd190dd1c9711e3a9cf2ca83d57
James Schumm <j.schumm@manchester.ac.uk>
2020-06-06 20:50:43 +0100

    Add header containing link to Leaflet CSS file
    Footer taken from CodeIgniter tutorial is not special to Leaflet Quick Start tutorial

---
+++ app/Views/templates/header.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html>
+<head>
+    <title>Leaflet Quick Start Guide</title>
+	<link rel="stylesheet" href="https://unpkg.com/leaflet@1.6.0/dist/leaflet.css"
+   integrity="sha512-xwE/Az9zrjBIphAcBb3F6JVqxf46+CDLwfLMHloNu6KEQCAWi6HcDUbeOfBIptF7tcCzusKFjFw2yuvEpDL9wQ=="
+   crossorigin=""/>
+   <style>
+   #mapid { height: 500px; }
+   </style>
+</head>
+<body>
+
+    <h1><?= esc($title); ?></h1>
