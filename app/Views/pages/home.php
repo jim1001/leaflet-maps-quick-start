59d05465369df6be1cfbd7a9848ce367596e774c
James Schumm <j.schumm@manchester.ac.uk>
2020-06-07 10:13:31 +0100

    Add some comments to JavaScript

--- app/Views/pages/home.php
+++ app/Views/pages/home.php
@@ -1,4 +1,8 @@
 <!-- Ref: https://leafletjs.com/examples/quick-start/ -->
+<!-- Access token for mapbox - see my keepass for mapbox signon
+
+
+
 
 <!-- Make sure you put this AFTER Leaflet's CSS -->
  <script src="https://unpkg.com/leaflet@1.6.0/dist/leaflet.js"
@@ -7,7 +11,10 @@
 
  <div id="mapid"> </div>
  <script>
+ /* Initialize the map and set its view to our chosen geographical coordinates and a zoom level */
  var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+ 
+ /*  Set the URL template for the tile images, the attribution text and the maximum zoom level of the layer */
  L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiamltMTAwMSIsImEiOiJja2IzdnY1NWEwOXdyMnpuc3o3aHBsNjh0In0.43ZJDfNGv3i360UR4WDd0g', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
