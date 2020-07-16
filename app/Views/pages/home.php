bf658a4d13afcf10da3eaaef1ec601c30756c072
James Schumm <j.schumm@manchester.ac.uk>
2020-07-16 16:05:01 +0100

    Change popup content using arrays and LayerGroups

--- app/Views/pages/home.php
+++ app/Views/pages/home.php
@@ -13,7 +13,7 @@
  <script>
  /* Initialize the map and set its view to our chosen geographical coordinates and a zoom level */
  var mymap = L.map('mapid').setView([51.505, -0.09], 13);
- 
+
  /*  Set the URL template for the tile images, the attribution text and the maximum zoom level of the layer */
  L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiamltMTAwMSIsImEiOiJja2IzdnY1NWEwOXdyMnpuc3o3aHBsNjh0In0.43ZJDfNGv3i360UR4WDd0g', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
@@ -25,38 +25,40 @@
 }).addTo(mymap);
 
 /* Add a marker */
-var marker1 = L.marker([51.5, -0.09]).addTo(mymap);
+var marker1 = L.marker([51.5, -0.09]);
 
 /* Add popups to objects added above */
 marker1.bindPopup("<b>Sample key: </b>BG-008<br><b>Selected phrase: </b>none");
+var myLayerGroup = L.layerGroup([marker1]);
 
 /* Add a marker */
-var marker2 = L.marker([51.52, -0.092]).addTo(mymap);
+var marker2 = L.marker([51.52, -0.092]);
 
 /* Add popups to objects added above */
-marker2.bindPopup("Marker2");
+marker2.bindPopup("<b>Sample key: </b>AB-100<br><b>Selected phrase: </b>none");
+myLayerGroup.addLayer(marker2);
+
+myLayerGroup.addTo(mymap);
+
+var phrases = ["rom1", "rom2"];
+
 </script>
 
-<div><p><button onclick="changePopUp()">Change popup</button></p></div>
+<div><p><button onclick="changePopUp(myLayerGroup, phrases)">Change popup</button></p></div>
 
 <script>
-function changePopUp() {
-    //marker1.setPopupContent("Marker1 changed");
-    var popup1 = marker1.getPopup();
-    var content1 = popup1.getContent();
-    //alert("marker1 popup says: " + content1)
-    var selPhrase="boy";
-    //Replace text "Selected phrase" to end of line with "Selected phrase..." + new phrase
-    var newContent = content1.replace(/Selected phrase.*$/, "Selected phrase: </b>"+ selPhrase)
-    popup1.setContent(newContent);
+function changePopUp(layerGroup_in, phrases_in) {
+  var popup;
+  var content;
+  var newContent
+  //alert(phrases_in);
+  var markers = layerGroup_in.getLayers();
+  for (var i = 0;i<markers.length;i++) {
+    popup = markers[i].getPopup();
+    content = popup.getContent();
+        //Replace text "Selected phrase" to end of line with "Selected phrase..." + new phrase
+    newContent = content.replace(/Selected phrase.*$/, "Selected phrase: </b>"+ phrases_in[i])
+    popup.setContent(newContent);
+  }
 }
 </script>
- 
- 
-
-
-
-
- 
-
- 
